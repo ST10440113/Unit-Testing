3182bf75ecbaebbc476f6851407008d5683bd01e
diya singh <161337050+ST10440113@users.noreply.github.com>
2024-05-17 15:02:19 +0200

    Update unit_testingTest.java

--- src/test/java/com/mycompany/automated_unit_testing/unit_testingTest.java
+++ src/test/java/com/mycompany/automated_unit_testing/unit_testingTest.java
@@ -21,7 +21,7 @@ public class unit_testingTest {
         //Create an instance of the class
         unit_testing ut = new  unit_testing();
         
-        int expected = 0;
+        int expected = 8;
         int  actual = ut.addition(4,4);
                 
         assertEquals(expected,actual);
@@ -32,7 +32,7 @@ public class unit_testingTest {
         //Create an instance of the class
      unit_testing ut = new  unit_testing();
      
-     int expected = 0;
+     int expected = 8;
      int actual = ut.subtraction(10,2);
      
      assertEquals(expected,actual);
@@ -43,7 +43,7 @@ public class unit_testingTest {
            //Create an instance of the class
      unit_testing ut = new  unit_testing();
      
-     String expected = "Hello World!";
+     String expected = "Hello World";
      String actual = ut.message();
      
      assertEquals(expected,actual);
